ce095cfe7a7b81b673a8c0a9948aa41cf5a348fa
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-04-03 23:37:11 -0500

    Added update function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,23 @@
-const usersStorage = require("../storages/usersStorage")
+const usersStorage = require("../storages/usersStorage.js")
+const { body, validationResult } = require("express-validator")
+
+const alphaErr = "must only contain letters."
+const lengthErr = "must be between 1 and 10 characters."
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+]
 
 const usersListGet = (req, res) => {
   res.render("index", {
@@ -13,14 +32,57 @@ const usersCreateGet = (req, res) => {
   })
 }
 
-const usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body
-  usersStorage.addUser({ firstName, lastName })
-  res.redirect("/")
+const usersCreatePost = [
+  // ⬅️ This is an array
+  validateUser, // First function (middleware)
+  (req, res) => {
+    // Second function (actual request handler)
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      })
+    }
+
+    const { firstName, lastName } = req.body
+    usersStorage.addUser({ firstName, lastName })
+
+    res.redirect("/")
+  },
+]
+
+const usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id)
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  })
 }
+
+const usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      })
+    }
+    const { firstName, lastName } = req.body
+    usersStorage.updateUser(req.params.id, { firstName, lastName })
+    res.redirect("/")
+  },
+]
 
 module.exports = {
   usersListGet,
   usersCreateGet,
   usersCreatePost,
+  usersUpdateGet,
+  usersUpdatePost,
 }
